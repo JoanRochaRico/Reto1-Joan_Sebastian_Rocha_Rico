6c83498c642a57f85de8a729978305968d201d5c
joan <joan.rocha.mt@correo.usa.edu.co>
2021-11-29 23:52:25 -0500

    first commit

---
+++ app/src/main/java/usa/sesion5/Reto1/MainActivity4.java
@@ -0,0 +1,14 @@
+package usa.sesion5.Reto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
